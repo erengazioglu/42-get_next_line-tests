282367cd4cab0f4c2706094759819988e6e52005
Eren Gazioglu <egaziogl@f6r8s5.clusters.42paris.fr>
2025-12-21 23:22:00 +0100

    start testing reads

--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 21:03:07 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/12/21 21:05:08 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/12/21 21:40:33 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int main(void)
 {
+	char	*result;
+	int	fd = open("test_file.txt", O_RDONLY);
+
 	printf("buffer size: %d\n", BUFFER_SIZE);
+	result = get_next_line(fd);
+	if (!result)
+		return (1);
+	printf("%s\n", result);
+	free(result);
+	return(0);
 }
--- tests.h
+++ tests.h
@@ -6,12 +6,13 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 21:03:27 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/12/21 21:04:46 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/12/21 21:18:05 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTS_H
 # define TESTS_H
 # include <stdio.h>
+# include <fcntl.h>
 # include "../get_next_line/get_next_line.h"
 #endif
