dad29f74d6fbe91ad875830a451457920fe4bae7
Eren <eren.g94@gmail.com>
2026-01-09 20:53:08 +0100

    start simplifying handle_lb

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:28:04 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/01/09 17:05:56 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/01/09 19:35:22 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int main(void)
 	printf("---\nTESTING buffer size %d\n", BUFFER_SIZE);
 	result = 0;
 	// result |= suite_strnjoin();
-	// result |= suite_read_multi(40, "testfile_regular.txt");
-	// result |= suite_read_multi(30, "testfile_startnl.txt");
-	// result |= suite_read_multi(15, "testfile_onlynl.txt");
-	// result |= suite_read_multi(5, "testfile_onenl.txt");
-	// result |= suite_read_multi(5, "testfile_onechar.txt");
-	// result |= suite_read_multi(5, "testfile_empty.txt");
+	result |= suite_read_multi(40, "testfile_regular.txt");
+	result |= suite_read_multi(30, "testfile_startnl.txt");
+	result |= suite_read_multi(15, "testfile_onlynl.txt");
+	result |= suite_read_multi(5, "testfile_onenl.txt");
+	result |= suite_read_multi(5, "testfile_onechar.txt");
+	result |= suite_read_multi(5, "testfile_empty.txt");
 	// result |= suite_read_multi(5, "invalid_file.txt");
-	result |= suite_read_multi(5, "43_with_nl.txt");
+	// result |= suite_read_multi(5, "43_with_nl.txt");
 	// result |= suite_read_multi_fd(5, -1);
 	// result |= suite_read_multi_fd(5, 0);
 	// result |= suite_read_multi_fd(5, 1);
